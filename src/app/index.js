b4760a1cde25a408b6f713f7c3b14d4997d88a86
Jonas Mosmann <jonasmosmann@web.de>
2021-04-06 15:11:29 +0200

    audio player new

--- src/app/index.js
+++ src/app/index.js
@@ -56,3 +56,22 @@ realFileBtn.addEventListener("change", function() {
   }
 });
 
+
+
+var $audio = $('#myAudio');
+$('input').on('change', function(e) {
+var target = e.currentTarget;
+var file = target.files[0];
+var reader = new FileReader();
+
+console.log($audio[0]);
+if (target.files && file) {
+var reader = new FileReader();
+reader.onload = function (e) {
+$audio.attr('src', e.target.result);
+$audio.play();
+}
+reader.readAsDataURL(file);
+}
+});
+
