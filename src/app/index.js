5de1a36e0c5787dbc9a21d3520db8fac6aff1e50
Tobias Huber <tobias.huber.oppenau@outlook.de>
2021-04-06 15:18:58 +0200

    performance changes

--- src/app/index.js
+++ src/app/index.js
@@ -33,5 +33,26 @@ function pauseButton(){
 	console.log("Transkription pausiert");
 	speechElement.stop();
 	document.getElementById('interim').innerHTML = "PAUSE";
-	
 }
+function stopButton(){
+	console.log("Transkription pausiert");
+	speechElement.stop();
+	document.getElementById('interim').innerHTML = "STOP";
+}
+
+const downloadToFile = (content, filename, contentType) => {
+	const a = document.createElement('a');
+	const file = new Blob([content], {type: contentType});
+	
+	a.href= URL.createObjectURL(file);
+	a.download = filename;
+	a.click();
+	
+	URL.revokeObjectURL(a.href);
+  };
+  
+  document.querySelector('#btnSave').addEventListener('click', () => {
+	const textArea = document.querySelector('textarea');
+	
+	downloadToFile(textArea.value, 'my-new-file.txt', 'text/plain');
+  });
