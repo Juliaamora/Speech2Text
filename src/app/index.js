2bce9246969d84f41c766329495379caaa8f2d85
Juliaamora <jul.morawietz@gmail.com>
2021-04-06 12:02:14 +0200

    feature for uploading files added

--- src/app/index.js
+++ src/app/index.js
@@ -35,3 +35,24 @@ function pauseButton(){
 	document.getElementById('interim').innerHTML = "PAUSE";
 	
 }
+
+
+
+const realFileBtn = document.getElementById("real-file");
+const customBtn = document.getElementById("custom-button");
+const customTxt = document.getElementById("custom-text");
+
+customBtn.addEventListener("click", function() {
+  realFileBtn.click();
+});
+
+realFileBtn.addEventListener("change", function() {
+  if (realFileBtn.value) {
+    customTxt.innerHTML = realFileBtn.value.match(
+      /[\/\\]([\w\d\s\.\-\(\)]+)$/
+    )[1];
+  } else {
+    customTxt.innerHTML = "No file chosen, yet.";
+  }
+});
+
